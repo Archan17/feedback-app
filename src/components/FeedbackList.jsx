b6e21ba491c0a011266ff39bbc9da2cb5bc36a1a
Archan Pandya <pandyaarchan17504@gmail.com>
2023-04-13 15:20:21 +0530

    Added Router About

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,3 +1,4 @@
+import {motion,AnimatePresence} from 'framer-motion'
 import Feedbackitem from "./Feedbackitem"
 import PropTypes from 'prop-types'
 
@@ -5,12 +6,32 @@ function FeedbackList({feedback,handleDelete}) {
     if(!feedback || feedback.length === 0){
         return <p>No feedback</p>
     }
-  return (
-    <div className='feedback-list'>
-        {feedback.map((item) => (<Feedbackitem key={item.id} item={item} handleDelete= {handleDelete} />
-        ))}
-    </div>
-  )
+
+    return (
+      <div className='feedback-list'>
+        <AnimatePresence>
+
+          {feedback.map((item) =>
+          <motion.div 
+            key={item.id}
+            initial={{opacity:0}}
+            animate={{opacity:1}}
+            exit={{opacity:0}}
+          > 
+          <Feedbackitem key={item.id} item={item} handleDelete= 
+            {handleDelete} />
+          </motion.div>
+          )}
+          </AnimatePresence>          
+      </div>
+    )
+
+    // return (
+    //   <div className='feedback-list'>
+    //       {feedback.map((item) => (<Feedbackitem key={item.id} item={item} handleDelete= {handleDelete} />
+    //       ))}
+    //   </div>
+    // )
 }
 
 FeedbackList.propTypes = {
